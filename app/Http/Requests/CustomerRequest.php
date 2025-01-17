a28d7846809a5429828d17f60eb851133625ade4
Ari P <ariteknologi@gmail.com>
2025-01-18 00:26:53 +0700

    add customer crud

---
+++ app/Http/Requests/CustomerRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CustomerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // only allow updates if the user is logged in
+        return backpack_auth()->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:5|max:100',
+            'email' => 'required|min:5|max:100|unique:customers,email,'.$this->id,
+            'phone' =>  'required|numeric|min_digits:5|max_digits:15',
+            'address' => 'required'
+        ];
+    }
+
+    /**
+     * Get the validation attributes that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
 
 class Customer extends Model
 {
+    use CrudTrait;
     protected $table = 'customers';
 
     protected $fillable = [
